fc54c2c3ba1d8e3b79af72a8783ad6ec94c53324
AH <awilliams.hintoso@gmail.com>
2022-09-11 13:09:09 -0700

    ps4a complete with recursion at 1:09pm

--- ps4/ps4a.py
+++ ps4/ps4a.py
@@ -3,8 +3,10 @@
 # Collaborators:
 # Time Spent: x:xx
 
+from itertools import permutations
+from math import perm
 from random import choice
-# import numpy as np
+import numpy as np
 
 def get_permutations(sequence):
     '''
@@ -25,64 +27,122 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
-    
-    # Non-Recursive Example
-    seq_copy = list(sequence)
-    seq_perm = []
-    perm_lib = [sequence]
 
-    def factorial(n):
-        if n == 1:
-            return 1
-        else:
-            return n * factorial(n-1)
+    # USE_RECURSION
+    permutations = []
 
-    while len(perm_lib) < factorial(len(sequence)):    
+    if len(sequence) == 1:
+        return list(sequence)
+    else: 
         for i in range(len(sequence)):
-            letter_choice = choice(seq_copy)
-            seq_perm.append(letter_choice)
-            seq_copy.remove(letter_choice)
+            new_additions = []
+            seq_copy = list(sequence) 
+            fixed_char = sequence[i]
+            seq_copy.remove(sequence[i])
+            
+            for items in get_permutations(seq_copy): 
+                new_additions.append(fixed_char + items)
+            
+            for new_strings in new_additions:
+                if new_strings not in permutations: 
+                    permutations.append(new_strings)
+
+    return permutations 
+    
+    # # Non-Recursive Example
+    # seq_copy = list(sequence)
+    # seq_perm = []
+    # perm_lib = [sequence]
+
+    # def factorial(n):
+    #     if n == 1:
+    #         return 1
+    #     else:
+    #         return n * factorial(n-1)
+
+    # while len(perm_lib) < factorial(len(sequence)):    
+    #     for i in range(len(sequence)):
+    #         letter_choice = choice(seq_copy)
+    #         seq_perm.append(letter_choice)
+    #         seq_copy.remove(letter_choice)
         
-        new_str = ''.join(seq_perm)
-        seq_copy = list(sequence)
-        seq_perm = []
+    #     new_str = ''.join(seq_perm)
+    #     seq_copy = list(sequence)
+    #     seq_perm = []
 
-        if new_str not in perm_lib: perm_lib.append(new_str)
+    #     if new_str not in perm_lib: perm_lib.append(new_str)
 
-    return print(perm_lib)
+    # return perm_lib
 
-    # if len(sequence) == factorial(len(sequence)): return print(sequence)
+    # Attempt at Recursive
+    # if len(sequence) == factorial(len(sequence[0])) and len(sequence) > 2 : return print(sequence)
+
+    # hist_str = []
 
     # if np.size(sequence) == 1:
+    #     hist_str = [sequence]
     #     wip_str = []
     #     seq_array = list(sequence)
     # elif len(sequence[-1]) < len(sequence[-2]):
+    #     # print(sequence[0])
+    #     # print(sequence[1])
+    #     for i in range(np.size(sequence)): 
+    #         hist_str.append(sequence[i])
     #     wip_str = list(sequence[-1])
-    #     for letters in wip_str: seq_array = list(sequence[-2]).remove(letters)     
+    #     prev_str = list(sequence[-2])
+    #     for letters in wip_str: prev_str.remove(letters)
+    #     seq_array = prev_str 
     # else: 
+    #     for i in range(np.size(sequence)):  
+    #         hist_str.append(sequence[i])
     #     wip_str = []
-    #     seq_array = list(sequence[-1])
+    #     seq_array = list(sequence[-1]) 
 
     # letter_choice = choice(seq_array)
 
-    # if len(seq_array) == 1: 
+    # if len(seq_array) == len(sequence[0]) and np.size(sequence) > 1: 
     #     wip_str.append(letter_choice)
     #     new_str = ''.join(wip_str)
+    #     del hist_str[-2]
     #     if new_str not in sequence: 
-    #         sequence = list(sequence).append(new_str)
-    #     else: del sequence[-1]
-    #     get_permutations(sequence)
-    # else:
-    #     seq_array.append(letter_choice)
+    #         hist_str.append(new_str)
+    #     else: del hist_str[-1] 
+    #     get_permutations(hist_str) 
+    # else: 
     #     wip_str.append(letter_choice)
     #     new_str = ''.join(wip_str)
-    #     sequence = list(sequence).append(new_str)
-    #     get_permutations(sequence) 
+    #     # if len(seq_array) > 1: del hist_str[-2]
+    #     hist_str.append(new_str)
+    #     get_permutations(hist_str) 
 
+#     permutations = []
 
-get_permutations("abc") 
+#     if len(sequence) == 1:
+#         return sequence
+#     else: 
+#         for i in range(len(sequence)):
+#             seq_copy = list(sequence)
+#             seq_copy.remove(sequence[i])
+#             this_loops_permutations = []  # 'a' + p('bc') == ['abc', 'acb']
+#             new_string_permutations = get_permutations(seq_copy)  # ['bc', 'cb']
+#             for each_permutation in new_string_permutations:
+#                 this_loops_permutations.append(sequence[i] + each_permutation)
+#             permutations.extend(this_loops_permutations)  # permutations.extend(['abc', 'acb'])
+#         return permutations
+    
 
+# sequence = 'abcc'
+# output = get_permutations(sequence)
+# print(f'our output: {output}')
 
+# itertools_output = [''.join(p) for p in permutations(sequence)]
+# print(f'itertools output: {itertools_output}')
+
+# print(f'matches: {output == itertools_output}')
+
+perm_lib = get_permutations("abcfgde")
+print(perm_lib,len(perm_lib))
+    
 if __name__ == '__main__':
 #    #EXAMPLE
 #    example_input = 'abc'
